d92f7f7e14cf54a43815bbcf8c87211c463c0701
ferna <ferna@LAPTOP-PNQUFFH0>
2018-09-06 23:56:23 -0500

    ya esta

--- ControlExcepciones/Form1.cs
+++ ControlExcepciones/Form1.cs
@@ -13,32 +13,79 @@ namespace ControlExcepciones
 
         private void btnEdad_Click(object sender, EventArgs e)
         {
-            try
+            bool except1=false;
+            do
             {
-                int edad = 2018 - int.Parse(txtbEdad.Text);
-                lbEdad.Text = "Tu edad es " + edad + " años.";
-                if (edad<1||edad>119)
+                except1 = false;
+
+                try
                 {
-                    string error = "Error: El año que ingresaste no es válido.";
-                    throw new ApplicationException(error);
+                    int edad = 2018 - int.Parse(txtbEdad.Text);
+                    lbEdad.Text = "Tu edad es " + edad + " años.";
+                    if (edad < 1 || edad > 119)
+                    {
+                        string error = "Error: El año que ingresaste no es válido.";
+                        throw new ApplicationException(error);
+                    }
+                }
+                catch (ApplicationException error)
+                {
+                    lbEdad.Text = error.Message;
+                    except1 = true;
+                }
+                catch (FormatException error)
+                {
+                    lbEdad.Text = "Error: " + error.Message;
+                    except1 = true;
+                }
+                catch
+                {
+                    lbEdad.Text = "Ups, parece que hubo un problema";
+                    except1 = true;
                 }
             }
-            catch (ApplicationException error)
-            {
-                lbEdad.Text = error.Message;
-            }
-            catch (FormatException error)
-            {
-                lbEdad.Text="Error: " + error.Message;
-            }
-            catch
-            {
-                lbEdad.Text = "Ups, parece que hubo un problema";
-            }
+            while (except1 == true);
+            
         }
 
         private void btnOperar_Click(object sender, EventArgs e)
         {
+            bool except2 = false;
+            do
+            {
+
+                except2 = false;
+
+                try
+                {
+
+                    int divisor = (2018-int.Parse(txtbEdad.Text))/ int.Parse(txtbDivisor.Text);
+
+                    lbDivision.Text = "La division es" + divisor;
+                    if (divisor == 0)
+                    {
+                        string error = "No se puede dividir";
+                        throw new ApplicationException(error);
+                    }
+                }
+                catch (ApplicationException error)
+                {
+                    lbDivision.Text = error.Message;
+                    except2 = true;
+                }
+                catch (FormatException error)
+                {
+                    lbDivision.Text = "Error" + error.Message;
+                    except2 = true;
+                }
+                catch
+                {
+                    lbDivision.Text = "Ups, parece que hubo un problema";
+                    except2 = true;
+                }
+            }
+            while (except2 == true);
+            
 
         }
     }
